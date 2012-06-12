6364e81aaf2f1fceda361fc867e7ef754cf600e3
Dru <drurly@gmail.com>
2012-06-12 13:28:38 -0400

    Added Files and Changed README

---
+++ README.md
@@ -0,0 +1,11 @@
+# Build Reddit in Sinatra
+
+## Quick Start 
+###Install Gems
+<pre>gem install sinatra sqlite3 dm-sqlite-adapter data_mapper sinatra-reloader haml</pre>
+###Clone Repo
+<pre>git clone git://github.com/DruRly/Build-Reddit-In-Sinatra.git</pre>
+###Start Server
+<pre>ruby reddit.rb</pre>
+
+## [Complete Tutorial](http://www.drurly.com/blog/2012/06/05/build-reddit-in-sinatra/)
---
+++ reddit.rb
@@ -0,0 +1,96 @@
+require 'sinatra'
+require 'data_mapper'
+require 'haml'
+require 'sinatra/reloader'
+
+DataMapper::setup(:default,"sqlite3://#{Dir.pwd}/example.db")
+
+class Link
+  include DataMapper::Resource
+  property :id, Serial
+  property :title, String 
+  property :url, Text 
+  property :score, Integer
+  property :points, Integer, :default => 0
+  property :created_at, Time
+
+  attr_accessor :score
+
+  def calculate_score
+  	time_elapsed = (Time.now - self.created_at) / 3600
+   	self.score = ((self.points-1) / (time_elapsed+2)**1.8).real
+  end
+
+  def self.all_sorted_desc
+    self.all.each { |item| item.calculate_score }.sort { |a,b| a.score <=> b.score }.reverse 
+  end
+end
+DataMapper.finalize.auto_upgrade!
+
+get '/' do 
+	@links = Link.all :order => :id.desc
+  haml :index
+end
+
+get '/hot' do
+	@links = Link.all_sorted_desc
+	haml :index	
+end
+
+post '/' do
+  l = Link.new
+  l.title = params[:title]
+  l.url = params[:url]
+  l.created_at = Time.now
+  l.save
+  redirect back
+end
+
+put '/:id/vote/:type' do 
+  l = Link.get params[:id]
+  l.points += params[:type].to_i
+  l.save
+  redirect back
+end 
+
+__END__
+
+@@ layout
+%html
+	%head
+		%link(rel="stylesheet" href="/css/bootstrap.css")
+		%link(rel="stylesheet" href="/css/style.css")
+	%body
+		.container
+			#main
+				.title Learn Sinatra
+				.options	
+					%a{:href => ('/')} New 
+					| 
+					%a{:href => ('/hot')} Hot
+				= yield
+
+@@ index
+#links-list	
+	-@links.each do |l|	
+		.row
+			.span3
+				%span.span
+					%form{:action => "#{l.id}/vote/1", :method => "post"}
+						%input{:type => "hidden", :name => "_method", :value => "put"}
+						%input{:type => "submit", :value => "⇡"}
+				%span.points
+					#{l.points}
+				%span.span
+					%form{:action => "#{l.id}/vote/-1", :method => "post"}
+						%input{:type => "hidden", :name => "_method", :value=> "put"}
+						%input{:type => "submit", :value => "⇣"}				
+			.span6
+				%span.link-title
+					%h3
+						%a{:href => (l.url)} #{l.title}
+#add-link
+	%form{:action => "/", :method => "post"}
+		%input{:type => "text", :name => "title", :placeholder => "Title"}
+		%input{:type => "text", :name => "url", :placeholder => "Url"}
+		%input{:type => "submit", :value => "Submit"}	

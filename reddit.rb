fda7202e4487bed22604999beea4a59b1109d8cb
nilved <nilved@nilved.me>
2012-06-28 21:10:27 -0400

    lazily evaluate + memoize score

--- reddit.rb
+++ reddit.rb
@@ -15,15 +15,15 @@ class Link
   property :points, Integer, :default => 0
   property :created_at, Time
 
-  attr_accessor :score
-
-  def calculate_score
-  	time_elapsed = (Time.now - self.created_at) / 3600
-   	self.score = ((self.points-1) / (time_elapsed+2)**1.8).real
+  def self.score
+    @_score ||= begin
+      time_elapsed = (Time.now - self.created_at) / 3600
+      ((self.points - 1) / (time_elapsed + 2) ** 1.8).real
+    end
   end
 
   def self.all_sorted_desc
-    self.all.each { |item| item.calculate_score }.sort { |a,b| a.score <=> b.score }.reverse 
+    self.all.sort { |a,b| a.score <=> b.score }.reverse 
   end
 end
 

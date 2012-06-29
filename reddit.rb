8b2d442be77d2ca1057f53ad2ccc9a3de15f40c5
nilved <nilved@nilved.me>
2012-06-28 22:44:48 -0400

    Pulled @nlived score change

--- reddit.rb
+++ reddit.rb
@@ -12,13 +12,20 @@ class Link
 
   attr_accessor :score
 
+  def score
+    @_score ||= begin
+      time_elapsed = (Time.now - self.created_at) / 3600
+      ((self.points - 1) / (time_elapsed + 2) ** 1.8).real
+    end
+  end
+
   def calculate_score
   	time_elapsed = (Time.now - self.created_at) / 3600
    	self.score = ((self.points-1) / (time_elapsed+2)**1.8).real
   end
 
   def self.all_sorted_desc
-    self.all.each { |item| item.calculate_score }.sort { |a,b| a.score <=> b.score }.reverse 
+    self.all.sort { |a,b| a.score <=> b.score }.reverse 
   end
 end
 DataMapper.finalize.auto_upgrade!
